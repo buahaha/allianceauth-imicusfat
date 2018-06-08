13a72a8a7e3b7a5b2149043d9e5478e0d1034ab0
Col Crunch <rhartnett35@gmail.com>
2018-06-08 17:34:40 -0400

    swagger spec
    No reason to have to manage another copy, just use the one used in allianceauth.eveonline

--- bfat/views.py
+++ bfat/views.py
@@ -1,5 +1,23 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
+import allianceauth.eveonline
+import os
+
+SWAGGER_SPEC_PATH = os.path.join(allianceauth.eveonline.__path__, 'swagger.json')
+"""
+Swagger Operations:
+
+get_characters_character_id_fleet
+get_fleets_fleet_id
+get_fleets_fleet_id_members
+get_characters_character_id
+get_universe_systems_system_id
+get_universe_stations_station_id
+get_universe_structures_structure_id
+get_universe_types_type_id
+"""
+
+
 
 # Create your views here.
 @login_required()
