d2f024d6c1d323e8a3f9c3df43c8c5a7e31ed03e
Col Crunch <rhartnett35@gmail.com>
2018-06-08 16:58:01 -0400

    Fix

--- bfat/auth_hooks.py
+++ bfat/auth_hooks.py
@@ -6,7 +6,7 @@ from allianceauth.services.hooks import MenuItemHook, UrlHook
 
 @hooks.register('menu_item_hook')
 def register_menu():
-    return MenuItemHook(_('Fleet Activity Tracking'), 'fas fa-crosshairs', 'bfat:bfat_view',
+    return MenuItemHook(_('Fleet Activity Tracking'), 'fa fa-crosshairs fa-fw', 'bfat:bfat_view',
                         navactive=['bfat:'])
 
 
