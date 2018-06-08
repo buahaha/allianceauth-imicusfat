e5d5c7d95a14984fd49b3d5710164d03ed296c42
Col Crunch <rhartnett35@gmail.com>
2018-06-08 16:51:24 -0400

    Oops.

--- bfat/admin.py
+++ bfat/admin.py
@@ -3,5 +3,5 @@ from django.contrib import admin
 from .models import Fat, FatLink
 
 # Register your models here.
-admin.site.register(Fatlink)
+admin.site.register(FatLink)
 admin.site.register(Fat)
