496fe6b28657338d41b1d2fadcd0047bb75077de
colcrunch <rhartnett35@gmail.com>
2018-07-09 02:29:06 -0400

    Cause words matter.

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
 # allianceauth-bfat
 
 A better FAT system for Alliance Auth.
+
+## Alliance Auth Settings (local.py)
+Paps vs. Fats... which do you prefer?
+
+Add the following line to your local.py settings if you would like to use PAP instead of FAT.
+```py
+fat_as_pap = True
+```
--- bfat/views.py
+++ bfat/views.py
@@ -2,8 +2,15 @@ from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 import allianceauth.eveonline
 import os
+from django.conf import settings
 
-# SWAGGER_SPEC_PATH = os.path.join(allianceauth.eveonline.__path__, 'swagger.json')
+
+if hasattr(settings, fat_as_pap):
+    term = 'PAP'
+else:
+    term = 'FAT'
+
+SWAGGER_SPEC_PATH = os.path.join(allianceauth.eveonline.__path__[0], 'swagger.json')
 """
 Swagger Operations:
 
