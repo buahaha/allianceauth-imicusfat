fcf2b48d1a4600d69375bb12602549a2aa73bab0
Col Crunch <rhartnett35@gmail.com>
2018-08-17 00:26:24 -0400

    Fix urls

--- bfat/urls.py
+++ bfat/urls.py
@@ -6,7 +6,7 @@ app_name = 'bfat'
 
 urlpatterns = [
     url(r'^$', views.bfat_view, name='bfat_view'),
-    url(r'^stats', views.stats, name='stats'),
-    url(r'^links', views.links, name='links'),
-    url(r'^links/add', views.add_link, name='link_add')
+    url(r'^stats/$', views.stats, name='stats'),
+    url(r'^links/$', views.links, name='links'),
+    url(r'^links/add/$', views.add_link, name='link_add')
 ]
