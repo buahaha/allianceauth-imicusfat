33194989e8bb54958d08a2de884a578e713b728d
Col Crunch <rhartnett35@gmail.com>
2018-08-17 00:48:22 -0400

    Ugh

--- bfat/urls.py
+++ bfat/urls.py
@@ -8,5 +8,5 @@ urlpatterns = [
     url(r'^$', views.bfat_view, name='bfat_view'),
     url(r'^stats/$', views.stats, name='stats'),
     url(r'^links/$', views.links, name='links'),
-    url(r'^links/add/$', views.add_link, name='link_add')
+    url(r'^links/add/$', views.add_link, name='bfat_link_add')
 ]
