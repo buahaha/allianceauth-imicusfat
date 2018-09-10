947327e448e9cafae79423d25868a14d8169b2c9
colcrunch <rhartnett35@gmail.com>
2018-09-10 00:54:24 -0400

    Fix Template

--- bfat/views.py
+++ bfat/views.py
@@ -133,7 +133,7 @@ def stats_corp(request, corpid, month=None, year=None):
         year = datetime.now().year
         months = []
         for i in range(1, 13):
-            corp_fats = Fat.objects.filter(character__corporation_id=corpid).count()
+            corp_fats = Fat.objects.filter(character__corporation_id=corpid, fatlink__fattime__month=i).count()
             if corp_fats is not 0:
                 months.append((i, corp_fats))
         ctx = {'term': term, 'corporation': corp.corporation_name, 'months': months, 'corpid': corpid, 'year': year}
