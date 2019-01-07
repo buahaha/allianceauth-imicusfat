344fc853be17524c7dd3a83568f2916cd8792c79
colcrunch <rhartnett35@gmail.com>
2019-01-07 03:48:22 -0500

    For better corp performance graphs, no need to use ints.

--- bfat/views.py
+++ bfat/views.py
@@ -286,7 +286,7 @@ def stats_alliance(request, allianceid, month=None, year=None):
     for corp in corporations:
         c_fats = fats.filter(character__corporation_id=corp.corporation_id).count()
         avg = c_fats/corp.member_count
-        data_avgs[corp.corporation_name] = int(avg)
+        data_avgs[corp.corporation_name] = round(avg, 2)
     data_avgs = OrderedDict(sorted(data_avgs.items(), key=lambda x: x[1], reverse=True))
     data_avgs = [list(data_avgs.keys()), list(data_avgs.values()), 'rgba({}, {}, {}, 1)'.format(random.randint(0, 255),
                                                                                            random.randint(0, 255),
