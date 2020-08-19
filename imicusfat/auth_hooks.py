5b770e0791020d68700091116bf533d054f4e2f1
Peter Pfeufer <info@ppfeufer.de>
2020-08-19 17:28:34 +0200

    [FIX] code formatting

--- imicusfat/auth_hooks.py
+++ imicusfat/auth_hooks.py
@@ -3,6 +3,7 @@ from django.utils.translation import ugettext_lazy as _
 from allianceauth import hooks
 from allianceauth.services.hooks import MenuItemHook, UrlHook
 
+
 @hooks.register('menu_item_hook')
 def register_menu():
     return MenuItemHook(_('Fleet Activity Tracking'), 'fas fa-crosshairs fa-fw', 'imicusfat:imicusfat_view',
