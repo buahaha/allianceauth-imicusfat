883c1f0441277208017c09711d63a662dfc0fd0b
Peter Pfeufer <info@ppfeufer.de>
2020-09-12 13:24:46 +0200

    Admin view optimized
    Model names and model view
    
    !! This change needs migrations to be run after applying !!

--- imicusfat/admin.py
+++ imicusfat/admin.py
@@ -5,6 +5,19 @@ from .models import IFat, IFatLink, IFatLinkType
 
 
 # Register your models here.
-admin.site.register(IFatLink)
-admin.site.register(IFat)
-admin.site.register(IFatLinkType)
+@admin.register(IFatLink)
+class IFatLinkAdmin(admin.ModelAdmin):
+    list_display = ("ifattime", "creator", "fleet", "link_type", "hash", "deleted_at")
+    ordering = ("-ifattime",)
+
+
+@admin.register(IFat)
+class IFatAdmin(admin.ModelAdmin):
+    list_display = ("character", "system", "shiptype", "ifatlink", "deleted_at")
+    ordering = ("-character",)
+
+
+@admin.register(IFatLinkType)
+class IFatLinkTypeAdmin(admin.ModelAdmin):
+    list_display = ("id", "name")
+    ordering = ("name",)
