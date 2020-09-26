938b5574262c4fa124d4123d1df54a49d90cb5fe
Peter Pfeufer <info@ppfeufer.de>
2020-09-26 16:20:40 +0200

    added some filter to the admin backend

--- imicusfat/admin.py
+++ imicusfat/admin.py
@@ -9,6 +9,17 @@ from django.contrib import admin
 from .models import IFat, IFatLink, IFatLinkType
 
 
+def custom_filter_title(title):
+    class Wrapper(admin.FieldListFilter):
+        def __new__(cls, *args, **kwargs):
+            instance = admin.FieldListFilter.create(*args, **kwargs)
+            instance.title = title
+
+            return instance
+
+    return Wrapper
+
+
 # Register your models here.
 @admin.register(IFatLink)
 class IFatLinkAdmin(admin.ModelAdmin):
@@ -20,12 +31,27 @@ class IFatLinkAdmin(admin.ModelAdmin):
         "ifattime",
         "creator",
         "fleet",
-        "link_type",
+        "_link_type",
         "is_esilink",
         "hash",
         "deleted_at",
     )
+
+    list_filter = (
+        "is_esilink",
+        ("link_type__name", custom_filter_title("fleet type")),
+    )
+
     ordering = ("-ifattime",)
+
+    def _link_type(self, obj):
+        if obj.link_type:
+            return obj.link_type.name
+        else:
+            return "-"
+
+    _link_type.short_description = "Fleet Type"
+    _link_type.admin_order_field = "link_type__name"
 
 
 @admin.register(IFat)
@@ -35,6 +61,9 @@ class IFatAdmin(admin.ModelAdmin):
     """
 
     list_display = ("character", "system", "shiptype", "ifatlink", "deleted_at")
+
+    list_filter = ("character", "system", "shiptype")
+
     ordering = ("-character",)
 
 
