3dece9cb7d6d848e76488dcf4ffe144d36489db4
Peter Pfeufer <info@ppfeufer.de>
2021-01-07 15:54:05 +0100

    version set to 1.4.0

--- imicusfat/__init__.py
+++ imicusfat/__init__.py
@@ -9,7 +9,7 @@ from django.utils.text import slugify
 default_app_config: str = "imicusfat.apps.ImicusfatConfig"
 
 __title__ = "ImicusFAT"
-__version__ = "1.3.1"
+__version__ = "1.4.0"
 __verbose_name__ = "ImicusFAT Fleet Activity Tracking for Alliance Auth"
 __user_agent__ = "{verbose_name} v{version} {github_url}".format(
     verbose_name=slugify(__verbose_name__, allow_unicode=True),
