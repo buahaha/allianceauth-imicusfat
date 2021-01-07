bcb5134b5def1b87cb3df83f93e110770550b54e
Peter Pfeufer <info@ppfeufer.de>
2021-01-07 16:09:37 +0100

    apparently we can't call django modules here

--- imicusfat/__init__.py
+++ imicusfat/__init__.py
@@ -4,15 +4,14 @@
 app config
 """
 
-from django.utils.text import slugify
-
 default_app_config: str = "imicusfat.apps.ImicusfatConfig"
 
 __title__ = "ImicusFAT"
 __version__ = "1.4.0"
 __verbose_name__ = "ImicusFAT Fleet Activity Tracking for Alliance Auth"
+__user_agent__name__ = "ImicusFAT-Fleet-Activity-Tracking-for-Alliance-Auth"
 __user_agent__ = "{verbose_name} v{version} {github_url}".format(
-    verbose_name=slugify(__verbose_name__, allow_unicode=True),
+    verbose_name=__user_agent__name__,
     version=__version__,
     github_url="https://gitlab.com/evictus.iou/allianceauth-imicusfat",
 )
