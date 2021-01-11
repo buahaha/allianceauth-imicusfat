146b2143f73160a2d0dfe9a2d68b1f7c3c2969db
Peter Pfeufer <info@ppfeufer.de>
2021-01-11 09:29:04 +0100

    we should import from the right module I guess

--- imicusfat/providers.py
+++ imicusfat/providers.py
@@ -7,7 +7,7 @@ providers
 from esi.clients import EsiClientProvider
 
 from imicusfat import __title__
-from aasrp.constants import USERAGENT
+from imicusfat.constants import USER_AGENT
 from imicusfat.utils import LoggerAddTag, get_swagger_spec_path
 
 from allianceauth.services.hooks import get_extension_logger
@@ -15,4 +15,4 @@ from allianceauth.services.hooks import get_extension_logger
 
 logger = LoggerAddTag(get_extension_logger(__name__), __title__)
 
-esi = EsiClientProvider(spec_file=get_swagger_spec_path(), app_info_text=USERAGENT)
+esi = EsiClientProvider(spec_file=get_swagger_spec_path(), app_info_text=USER_AGENT)
